9f092fc8b762d4e57d3b5b9d3d4d0e745794c0e0
Dijo-7 <74957558+Dijo-7@users.noreply.github.com>
2022-09-19 05:12:13 -0700

    Update leakybucket.c

--- leakybucket.c
+++ leakybucket.c
@@ -12,22 +12,22 @@ void main()
 	scanf("%d",&b);
 	printf("Enter the output size:");
 	scanf("%d",&o);
-    while (n != 0) 
+   	while (n != 0) 
 	{
-        printf("Enter the incoming packet size : ");
-        scanf("%d", &i);
-        printf("Incoming packet size %d\n", i);
-        if (i <= (b - bucket)){
-            bucket += i;
-            printf("Bucket buffer size %d out of %d\n", bucket, b);
-        } else {
-            printf("Dropped %d no of packets\n", i - (b - bucket));
-            printf("Bucket buffer size %d out of %d\n", bucket, b);
-            bucket = b;
-        }
-        bucket = bucket - o;
-        printf("After outgoing %d packets left out of %d in buffer\n", bucket, b);
-        n--;
+       		printf("\nEnter the incoming packet size : ");
+       		scanf("%d", &i);
+        	if (i <= (b - bucket)){
+            		bucket += i;
+            		printf("Bucket buffer size %d out of %d\n", bucket, b);
+        	} 
+		else {
+            		printf("Dropped %d no of packets\n", i - (b - bucket));
+           		printf("Bucket buffer size %d out of %d\n", bucket, b);
+           		 bucket = b;
+        	}
+       		bucket = bucket - o;
+        	printf("After outgoing %d packets left out of %d in buffer\n", bucket, b);
+        	n--;
 	}
 	
 }
